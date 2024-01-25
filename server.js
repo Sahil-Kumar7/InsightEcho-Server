e92e3b2d33fd9d7a9dba797e615af29779f07ec0
sahil <sahil1223.be21@chitkara.edu.in>
2024-01-25 13:41:03 +0530

    second commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-app.use(cors({credentials: true, origin: "http://localhost:3000"}))
+app.use(cors({credentials: true, origin: "https://insight-echo-client.vercel.app/"}))
 app.use(upload());
 // app.use('/uploads', express.static(__dirname + '/uploads'))
 
